5fa4a86e6fcc71c085e2d393c3652571ead647dd
227735 <227735@student.uek.krakow.pl>
2023-10-16 21:48:15 +0200

    zadanie

---
+++ 02-ClassesAndObjects/program.java
@@ -0,0 +1,18 @@
+public class program {
+  public static void main(String[] args) {
+    String name = "Monika";
+    String surname = "Kłosowska";
+    int age = 20;
+    String ID = "227735";
+    int year = 2;
+    String field_of_study = "Informatyka Stosowana";
+    String university = "Uniwersytet Ekonomiczny w Krakowie";
+    System.out.println(name);
+    System.out.println(surname);
+    System.out.println(age);
+    System.out.println(ID);
+    System.out.println(year);
+    System.out.println(field_of_study);
+    System.out.println(university);
+  }
+}
